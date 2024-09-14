7bb37df37af2a1fae9d1f249537350dc86aa9c5d
Cloud User <ec2-user@ip-172-31-8-215.ap-south-1.compute.internal>
2024-09-14 15:42:37 +0000

    Initial version of AWS resource tracker script

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#####################
+# Author: Rinku
+# Date: Sep14, 24
+# Version: v1.1
+# This script will report the AWS resource usage
+#####################
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+
+# list s3 buckets
+echo "Print list of s3 buckets"
+aws s3 ls
+
+# list EC2 instances
+echo "Print list of ec2 instances"
+aws ec2 describe-instances
+
+# list lambda
+echo "Print list of lambda functios"
+aws lambda list-functions
+
+# list IAM users
+echo "Print list of iam users"
+aws iam list-users
