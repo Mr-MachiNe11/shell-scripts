0baafa422edde5e0ab678dcdd18d2ff622e9db55
Cloud User <ec2-user@ip-172-31-8-215.ap-south-1.compute.internal>
2024-09-15 04:57:48 +0000

    Updated script for cronjob

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -3,7 +3,7 @@
 #####################
 # Author: Rinku
 # Date: Sep14, 24
-# Version: v1.2
+# Version: v1.3
 # This script will report the AWS resource usage
 #####################
 
@@ -12,18 +12,21 @@
 # AWS Lambda
 # AWS IAM Users
 
+# Define the output file with a timestamp
+OUTPUT_FILE="/home/ec2-user/aws_resource_report_$(date +'%Y-%m-%d_%H-%M-%S').log"
+
 # list s3 buckets
-echo "Print list of s3 buckets"
-aws s3 ls
+echo "[$(date)] Print list of s3 buckets" >> $OUTPUT_FILE
+aws s3 ls >> $OUTPUT_FILE
 
 # list EC2 instances
-echo "Print list of ec2 instances"
-aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+echo "[$(date)] Print list of ec2 instances" >> $OUTPUT_FILE
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' >> $OUTPUT_FILE
 
 # list lambda
-echo "Print list of lambda functios"
-aws lambda list-functions
+echo "[$(date)] Print list of lambda functios" >> $OUTPUT_FILE
+aws lambda list-functions >> $OUTPUT_FILE
 
 # list IAM users
-echo "Print list of iam users"
-aws iam list-users
+echo "[$(date)] Print list of iam users" >> $OUTPUT_FILE
+aws iam list-users >> $OUTPUT_FILE
