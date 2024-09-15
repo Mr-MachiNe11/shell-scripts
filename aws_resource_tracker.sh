0227feea7bc26de2339df0e1603fd0238cb0e644
Cloud User <ec2-user@ip-172-31-8-215.ap-south-1.compute.internal>
2024-09-15 09:18:27 +0000

    Changed into the output file

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -13,20 +13,20 @@
 # AWS IAM Users
 
 # Define the output file with a timestamp
-OUTPUT_FILE="/home/ec2-user/aws_resource_report_$(date +'%Y-%m-%d_%H-%M-%S').log"
+OUTPUT_FILE="/home/ec2-user/aws_resource_report/aws_resource_report.log"
 
 # list s3 buckets
-echo "[$(date)] Print list of s3 buckets" >> $OUTPUT_FILE
+echo "Print list of s3 buckets" >> $OUTPUT_FILE
 aws s3 ls >> $OUTPUT_FILE
 
 # list EC2 instances
-echo "[$(date)] Print list of ec2 instances" >> $OUTPUT_FILE
+echo "Print list of ec2 instances" >> $OUTPUT_FILE
 aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' >> $OUTPUT_FILE
 
 # list lambda
-echo "[$(date)] Print list of lambda functios" >> $OUTPUT_FILE
+echo "Print list of lambda functios" >> $OUTPUT_FILE
 aws lambda list-functions >> $OUTPUT_FILE
 
 # list IAM users
-echo "[$(date)] Print list of iam users" >> $OUTPUT_FILE
+echo "Print list of iam users" >> $OUTPUT_FILE
 aws iam list-users >> $OUTPUT_FILE
